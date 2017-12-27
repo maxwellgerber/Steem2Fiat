71eb92b94584d8835464bc51e4a051acf1596f3c
Maxwell Gerber <mgerber@berkeley.edu>
2017-12-26 20:41:37 -0800

    Cleanup

--- background.js
+++ background.js
@@ -148,8 +148,6 @@ class SettingsManager{
   }
 
   set(key,val) {
-    console.log(val);
-    console.log(JSON.stringify(val));
     localStorage.setItem(key, JSON.stringify(val)); 
   }
 
@@ -158,15 +156,9 @@ class SettingsManager{
       var stored = this.get("steem_sbd_ratio");
       if (stored === undefined || IsExpired(stored.timestamp)) {
         RPCSteemSbdRatio().then(data => {
-          console.log(data);
-          console.log(typeof data);
-          console.log(data.result);
           var temp = data.result.base;
-          console.log(temp);
           temp = temp.substring(0, temp.length-4);
-          console.log(temp);
           temp = Number(temp);
-          console.log(temp);
           var obj = {value: temp, timestamp: Date.now()};
           this.set("steem_sbd_ratio", obj);
           resolve(obj.value); 
---
+++ content_scripts/dtube.js
@@ -0,0 +1,43 @@
+
+var settings = {
+	selectors: "div.videosnaprest",
+	css:"height=16.29 width=16.29 style=\"position:relative; top:3px\""
+}
+
+function crawlPage() {
+	var imgurl = "https://steemit-production-imageproxy-upload.s3.amazonaws.com/DQmPBTSoFeabenhqLDu3oYa2sxZCpCGn6dQPvaqBuwZSdyA";
+	$(settings.selectors).each(function() {
+		var payout = this.dataset.s2f;
+		if (payout === undefined) {
+			payout = parseFloat(this.textContent.replace(",", "").replace("$", ""));
+			this.dataset.s2f = payout;
+		}
+		payout = Number(payout);
+	  var fiat = (payout*settings.convert_rate).toLocaleString(navigator.language, {maximumFractionDigits:2});
+	  var payout = payout.toLocaleString(navigator.language, {maximumFractionDigits:2});
+	  var innerHTML = `
+	  <span>
+	    ${settings.symbol}${fiat}&nbsp;<img src="${imgurl}" ${settings.css}">${payout}
+	  </span>
+	  `;
+	  $(this).html(innerHTML);
+	});
+}
+
+function crawlDelay(){
+	setTimeout(crawlPage, 350);
+};
+
+function init() {
+	chrome.runtime.sendMessage({msg: "request_display_info"}, function(response) {
+		settings.convert_rate = response.rate
+		settings.symbol = response.symbol;
+		crawlPage();
+		// Idempotent
+		window.addEventListener("scroll", crawlDelay);
+		$("a").click(crawlDelay);
+	});
+}
+init();
+
+chrome.extension.onMessage.addListener(init);
