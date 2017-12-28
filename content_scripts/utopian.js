cb521a426ab7c80699197a7ebbfb52ecd7ed0947
Maxwell Gerber <mgerber@berkeley.edu>
2017-12-27 16:11:12 -0800

    Major performance overhaul

--- content_scripts/utopian.js
+++ content_scripts/utopian.js
@@ -3,41 +3,44 @@ var settings = {
 	selectors: "span.Payout, span.CommentFooter__payout",
 	css:"height=16.29 width=16.29 style=\"position:relative; top:3px\""
 }
+var imgurl = chrome.runtime.getURL("img/steem_icon.png");
 
 function crawlPage() {
-	var imgurl = "https://steemit-production-imageproxy-upload.s3.amazonaws.com/DQmPBTSoFeabenhqLDu3oYa2sxZCpCGn6dQPvaqBuwZSdyA";
-	$(settings.selectors).each(function() {
-		var payout = this.dataset.s2f;
-		if (payout === undefined) {
-			payout = parseFloat(this.textContent.replace(",", "").replace("$", ""));
-			this.dataset.s2f = payout;
-		}
-		payout = Number(payout);
-	  var fiat = (payout*settings.convert_rate).toLocaleString(navigator.language, {maximumFractionDigits:2});
-	  var payout = payout.toLocaleString(navigator.language, {maximumFractionDigits:2});
-	  var innerHTML = `
+    var elements = $(settings.selectors);
+    elements.each(function() {
+        var payout = this.dataset.s2f;
+        if (payout === undefined) {
+            payout = parseFloat(this.textContent.replace(",", "").replace("$", ""));
+            this.dataset.s2f = payout;
+        }
+        payout = Number(payout);
+        var fiat = (payout * settings.convert_rate).toLocaleString(navigator.language, {
+            maximumFractionDigits: 2, minimumFractionDigits: 2
+        });
+        var payout = payout.toLocaleString(navigator.language, {
+            maximumFractionDigits: 2
+        });
+        var innerHTML = `
 	  <span>
 	    ${settings.symbol}${fiat}&nbsp;<img src="${imgurl}" ${settings.css}>${payout}
 	  </span>
 	  `;
-	  $(this).html(innerHTML);
-	});
+        $(this).html(innerHTML);
+    });
 }
-
-function crawlDelay(){
-	setTimeout(crawlPage, 350);
-};
 
 function init() {
-	chrome.runtime.sendMessage({msg: "request_display_info"}, function(response) {
-		settings.convert_rate = response.rate
-		settings.symbol = response.symbol;
-		crawlPage();
-		// Idempotent
-		window.addEventListener("scroll", crawlDelay);
-		$("div").click(crawlDelay);
-	});
+    chrome.runtime.sendMessage({
+        msg: "request_display_info"
+    }, function(response) {
+        settings.convert_rate = response.rate
+        settings.symbol = response.symbol;
+        crawlPage();
+    });
 }
-init();
 
-chrome.extension.onMessage.addListener(init);
+init();
+setInterval(crawlPage, 500);
+chrome.extension.onMessage.addListener(()=>{
+    last_seen = 0;
+});
